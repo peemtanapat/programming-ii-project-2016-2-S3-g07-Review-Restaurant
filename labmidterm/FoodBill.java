cdf69cf7bbb6078d3ad134ebaf65f1b7d69ff44f
S.Natthakit <natthakit.iamnutz@mail.kmutt.ac.th>
2017-03-10 14:24:39 +0700

    bill

--- labmidterm/FoodBill.java
+++ labmidterm/FoodBill.java
@@ -9,6 +9,20 @@ package labmidterm;
  *
  * @author USER
  */
-public class FoodBill {
+public class FoodBill extends GoodsBill {
+
+    public FoodBill(Customer cust,double price) {
+        this.cust = cust;
+        this.price = price;
+        count++;
+    }
     
+    public long getNumOfBill() {
+        return count;
+    }
+    
+    public String toString() {
+        return "Food Bill, Bill id: "+count+" Customer id: "+custId+"\nTotal Price = "+getTotalPrice+", Promotion Price="+promoPrice+"\nNet : "+(getTotalprice-promoPrice);
+    }
 }
+
--- labmidterm/GoodsBill.java
+++ labmidterm/GoodsBill.java
@@ -7,8 +7,14 @@ public class GoodsBill extends Bill {
     public GoodsBill(Customer cust,double price) {
         this.cust = cust;
         this.price = price;
+        count++;
     }
     
+    public long getNumOfBill() {
+        return count;
+    }
     
-    
+    public String toString() {
+        return "Goods Bill, Bill id: "+count+" Customer id: "+custId+"\nTotal Price = "+getTotalPrice+", Promotion Price="+promoPrice+"\nNet : "+(getTotalprice-promoPrice);
+    }
 }
