b2609f16eb39502aa207df4367dc516d532d1409
S.Natthakit <natthakit.iamnutz@mail.kmutt.ac.th>
2017-03-10 16:27:20 +0700

    no 4

---
+++ No4/Application.java
@@ -0,0 +1,13 @@
+
+package No4;
+
+public class Application extends Item {
+    
+    public Application (String appName, String category, double price) {
+        super();
+        this.appName = appName;
+        this.category = category;
+        this.price = price;
+    }
+    
+}
---
+++ No4/Movie.java
@@ -0,0 +1,12 @@
+
+package No4;
+
+public class Movie extends Item {
+    
+    public Movie (String appName, String category, double price) {
+        super();
+        this.appName = appName;
+        this.category = category;
+        this.price = price;
+    }
+}
