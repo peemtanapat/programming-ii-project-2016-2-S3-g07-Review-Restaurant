955e903478ce375f17bf84c28012e61bd8224cc7
S.Natthakit <natthakit.iamnutz@mail.kmutt.ac.th>
2017-03-10 14:21:33 +0700

    Add files via upload
    บิลอาหาร + บิลสินค้า

---
+++ labmidterm/GoodsBill.java
@@ -0,0 +1,14 @@
+
+package labmidterm;
+
+public class GoodsBill extends Bill {
+    private long count;
+
+    public GoodsBill(Customer cust,double price) {
+        this.cust = cust;
+        this.price = price;
+    }
+    
+    
+    
+}
