1c3974f89bbef5170bf65c4f6d4166b32b60721c
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2017-03-19 21:10:03 +0700

    สำรองข้อมูล

---
+++ 09032017(สำรอง)/User.java
@@ -0,0 +1,15 @@
+package Project_Review;
+
+public class User {
+
+    private String userName;
+    
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public String toString() {
+        return "Review by: " + userName;
+    }
+}
