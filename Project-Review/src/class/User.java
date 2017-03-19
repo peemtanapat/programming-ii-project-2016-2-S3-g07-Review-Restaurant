7eb9dd39ee515d86088591aec38440f78a894e11
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2017-03-19 18:45:08 +0700

    เพิ่ม(0-5)ในคลาส Test ในการกรอกคะแนน

---
+++ Project-Review/src/class/User.java
@@ -0,0 +1,15 @@
+package projectjava;
+
+public class User {
+
+    private String userName;
+    
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public String toString() {
+        return "Review by: " + userName;
+    }
+}
